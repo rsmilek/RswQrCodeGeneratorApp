a36989bb1434f6fa9294d5b1174c9825f49eaf4e
rsmilek <smilek.radim@gmail.com>
2023-12-22 10:19:25 +0100

    Fixed imports in API module.

--- src/app/shared/qr-code-generator-api/qr-code-generator-api.module.ts
+++ src/app/shared/qr-code-generator-api/qr-code-generator-api.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { QrCodeGeneratorApiService } from './qr-code-generator-api.service';
 
 @NgModule({
   declarations: [],
+  imports: [
+    CommonModule, 
+    HttpClientModule
+  ],
   providers: [
     QrCodeGeneratorApiService
   ]
--- src/app/shared/qr-code-generator-api/qr-code-generator-api.service.ts
+++ src/app/shared/qr-code-generator-api/qr-code-generator-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs'
-import { UrlDTO } from './UrlDTOs/UrlDTO';
+import { UrlDTO } from './DTOs/UrlDTO';
 
 @Injectable({
   providedIn: 'root'
