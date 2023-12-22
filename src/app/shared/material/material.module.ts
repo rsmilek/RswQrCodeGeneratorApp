e891ffe860f12a715e9fdee6f8f7e3edd3889cc7
rsmilek <smilek.radim@gmail.com>
2023-12-22 21:43:36 +0100

    Added Angular material into html

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
   exports: [
     MatButtonModule,
-    MatCheckboxModule
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule
   ]
 })
 export class MaterialModule { }
