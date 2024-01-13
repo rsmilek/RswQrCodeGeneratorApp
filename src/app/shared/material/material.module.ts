615d48d5236ea626f54409237b865adcf8888657
rsmilek <smilek.radim@gmail.com>
2024-01-13 10:38:24 +0100

    Added card from Android Material

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
@@ -13,6 +14,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   declarations: [],
   exports: [
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
     MatInputModule,
     MatListModule,
