8394fe0be62c5c69792bb95724f84782af9cd4da
Radim SMILEK <radim.smilek@cz.abb.com>
2026-01-25 05:43:19 +0100

    Added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rswqrcodegenerator.app',
+  appName: 'QR Code Gen',
+  webDir: 'dist/rsw-qr-code-generator-app/browser'
+};
+
+export default config;
