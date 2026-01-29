76f2e11bc142024a5e6efaedade245e38af03e3e
Radim SMILEK <radim.smilek@cz.abb.com>
2026-01-29 09:16:49 +0100

    Bundle optimalization: Angular material

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -17,13 +17,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   exports: [
     MatButtonModule,
     MatCardModule,
+    MatDividerModule,
+    MatFormFieldModule,
     MatIconModule,
     MatInputModule,
     MatListModule,
-    MatFormFieldModule,
     MatProgressSpinnerModule,
     MatSidenavModule,
-    MatSlideToggleModule,
     MatSnackBarModule,
     MatToolbarModule
   ]
